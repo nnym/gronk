99bb331b79b1269ba63d091a97fb01274ca4ae52
Mohamed Maaliki <me@auoeke.net>
2021-12-25 19:30:43 +0000

    fix extension `maven` usable only once

--- source/net/auoeke/gronk/MavenRepositoryExtension.java
+++ source/net/auoeke/gronk/MavenRepositoryExtension.java
@@ -1,10 +1,13 @@
 package net.auoeke.gronk;
 
 import groovy.lang.Closure;
+import java.util.List;
+import org.gradle.api.Action;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.dsl.RepositoryHandler;
 import org.gradle.api.artifacts.repositories.MavenArtifactRepository;
 
+@SuppressWarnings("unused")
 public class MavenRepositoryExtension extends Closure<Void> {
     private final Project project;
 
@@ -14,15 +17,21 @@ public class MavenRepositoryExtension extends Closure<Void> {
         this.project = project;
     }
 
-    public MavenArtifactRepository doCall(Object url, Closure configure) {
+    public MavenArtifactRepository doCall(Object url, Action<MavenArtifactRepository> configure) {
         return this.repositories().maven(repository -> {
             repository.setUrl(url);
-            this.project.configure(repository, configure);
+            repository.setName(url.toString());
+            this.project.configure(List.of(repository), configure);
+
+            if (repository.getName().equals("maven")) {
+                // Panic because further attempts to access this extension will fail.
+                throw new IllegalArgumentException("\"maven\" is a bad repository name.");
+            }
         });
     }
 
     public MavenArtifactRepository doCall(Object url) {
-        return this.repositories().maven(repository -> repository.setUrl(url));
+        return this.doCall(url, repository -> {});
     }
 
     private RepositoryHandler repositories() {
